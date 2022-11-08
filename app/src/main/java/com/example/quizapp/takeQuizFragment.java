0faa7f433ad688a5aba64fb9ea88c64b6f90d748
satake91t <stk1293blue@gmail.com>
2022-11-08 15:56:21 +0900

    copy file

---
+++ app/src/main/java/com/example/quizapp/takeQuizFragment.java
@@ -0,0 +1,58 @@
+package com.example.quizapp;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class takeQuizFragment extends Fragment {
+
+    private int[] order={1,2,3,4};
+    //takeQuizPackActivity内変数
+    private takeQuizPackActivity tqActivity;
+    private int correctNum;
+    //mainApplication内変数
+    private mainApplication mainApp;
+    private String packId;
+    private int quizNum;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
+        super.onCreateView(inflater, container, savedInstance);
+        return inflater.inflate(R.layout.fragment_take_quiz, container, false);
+    }
+
+    @Override
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //ファイル読み込み用変数
+        List<String> lines = new ArrayList<>();
+
+        String[] quizData;
+
+        //外部クラスのデータ取得
+        this.tqActivity = (takeQuizPackActivity)getActivity();
+        this.correctNum = this.tqActivity.getCorrectNum();
+        this.mainApp = tqActivity.getMainApplication();
+        this.packId = this.mainApp.getPackId();
+        this.quizNum = mainApp.getQuizNum();
+
+        //ファイル読み込み
+        lines = mainApp.readFileAsList(packId);
+        //quizData = readQuizData(this.quizNum);
+    }
+
+    /*
+    public String[] readQuizData(quizNum){
+
+    }*/
+}
---
+++ app/src/main/java/com/example/quizapp/takeQuizPackActivity.java
@@ -0,0 +1,98 @@
+package com.example.quizapp;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class takeQuizPackActivity extends AppCompatActivity {
+
+    private int correctNum;
+    private FragmentTransaction transaction;
+    public mainApplication mainApp;
+
+    //Activity起動時, パック選択画面を表示するメソッド
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_quiz_pack);
+
+        //画面遷移用transaction初期化
+        transaction = getSupportFragmentManager().beginTransaction();
+
+        //メニューボタン作成
+        Button btnMenu = findViewById(R.id.btnMenu);
+        btnMenu.setOnClickListener(v -> finish());
+
+        //パック選択画面起動
+        this.selectPack();
+    }
+
+    //パック作成画面を表示するメソッド
+    public void selectPack(){
+        selectPackFragment selectPackFragment = new selectPackFragment();
+        transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.container, selectPackFragment);
+        transaction.commit();
+    }
+
+    //クイズ回答画面を表示するメソッド
+    public void takeQuiz(){
+        this.correctNum = 0;
+        takeQuizFragment takeQuizFragment = new takeQuizFragment();
+        transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.container, takeQuizFragment);
+        transaction.commit();
+    }
+
+    //結果を表示するメソッド
+    public void result(){
+        resultFragment resultFragment = new resultFragment();
+        transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.container, resultFragment);
+        transaction.commit();
+    }
+
+    //遷移先のFragmentからメニューボタンを再表示させるメソッド
+    public LinearLayout getLinearLayout() {
+        LinearLayout layout = null;
+        Button button = new Button(this);
+        button.setTextSize(10);
+        button.setText("メニュー");
+        button.setOnClickListener(backMainActivity);
+        LinearLayout.LayoutParams buttonLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        button.setLayoutParams(buttonLayoutParams);
+        layout.addView(button);
+
+        return layout;
+    }
+    //再作成したメニューボタンの処理
+    View.OnClickListener backMainActivity = new View.OnClickListener(){
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        public void onClick(View v){
+            finish();
+        }
+    };
+
+    //mainApplicationのgetter
+    public mainApplication getMainApplication(){
+        return mainApp;
+    }
+
+    //correctNumのgetterとsetter
+    public int getCorrectNum() {
+        return correctNum;
+    }
+    public void setCorrectNum(int correctNum){
+        this.correctNum = correctNum;
+    }
+
+}
