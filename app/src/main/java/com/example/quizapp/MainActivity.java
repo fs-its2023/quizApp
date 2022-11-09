b5029c6ec82db52e19b000954c6f659bf35fba79
sasagawayosuke <sukeyou5050@gmail.com>
2022-11-09 13:26:29 +0900

    sasatest

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -24,12 +24,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //テス
-<<<<<<< HEAD
+
         //ささがわです
 
         //岡部>_<
-=======
+
         //岡部
->>>>>>> 5b2f37232e5fe9bfaea186c0b51643d877e04dfd
+
     }
 }
