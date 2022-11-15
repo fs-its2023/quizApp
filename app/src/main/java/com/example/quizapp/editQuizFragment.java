ac05a8f66477e62f02d6ef648df9e8829741c047
sasagawayosuke <sukeyou5050@gmail.com>
2022-11-15 14:21:26 +0900

    ちょっと追加しました

--- app/src/main/java/com/example/quizapp/editQuizFragment.java
+++ app/src/main/java/com/example/quizapp/editQuizFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
@@ -13,7 +15,20 @@ import androidx.fragment.app.Fragment;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class editQuizFragment extends Fragment {
+    View layout;
+    private Button btnSaveAndNext;
+    private Button btnSaveAndExit;
+    private EditText editTxtQuizSentence;
+    private EditText editTxtCorrectOption;
+    private EditText editTxtIncorrectOption1;
+    private EditText editTxtIncorrectOption2;
+    private EditText editTxtIncorrectOption3;
+    private EditText editTxtQuizExplanation;
+    private TextView txtPackTitle;
+
 
     //MainApplicationの取得
     //MainActivity MainActivity=(MainActivity)getActivity();
@@ -28,6 +43,7 @@ public class editQuizFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         super.onCreateView(inflater, container, savedInstanceState);
+        View layout = inflater.inflate(R.layout.fragment_edit_quiz,container,false);
         return inflater.inflate(R.layout.fragment_edit_quiz,container,false);
     }
 
@@ -38,6 +54,16 @@ public class editQuizFragment extends Fragment {
 
         makePackActivity =(makePackActivity)getActivity();
         mainApplication=(com.example.quizapp.mainApplication) makePackActivity.getMainApplication();
+
+        Button btnSaveAndNext=layout.findViewById(R.id.btnSaveAndNext);
+        Button btnSaveAndExit=layout.findViewById(R.id.btnSaveAndExit);
+        EditText editTxtQuizSentence=layout.findViewById(R.id.editTxtQuizSentence);
+        EditText editTxtCorrectOption=layout.findViewById(R.id.editTxtCorrectOption);
+        EditText editTxtIncorrectOption1=layout.findViewById(R.id.editTxtIncorrectOption1);
+        EditText editTxtIncorrectOption2=layout.findViewById(R.id.editTxtIncorrectOption2);
+        EditText editTxtIncorrectOption3=layout.findViewById(R.id.editTxtIncorrectOption3);
+        EditText editTxtQuizExplanation=layout.findViewById(R.id.editTxtQuizExplanation);
+        TextView txtPackTitle=layout.findViewById(R.id.txtPackTitle);
 
         //新規か編集かの判定
         if(makePackActivity.getQuizTotalNum()==0){
@@ -53,6 +79,12 @@ public class editQuizFragment extends Fragment {
             String[] strLastRowAllList = allList.get(allList.size()-1).split(",");
             String newPackId = String.valueOf(Integer.parseInt(strLastRowAllList[0])+1);
             mainApplication.setPackId(newPackId);
+
+            //パックタイトルの表示:makeNewPackで設定された値をとってくる
+            //タイトル、説明文、ジャンル
+            String packTitle = this.makePackActivity.getPackTitle();
+            String packIntroduction = this.makePackActivity.getPackIntroduction();
+            String packGenre = this.makePackActivity.getPackGenre();
         }
 
         //編集だった場合。入力欄に保存されていた情報を表示する、mainApplicationからpackIdを取得する
