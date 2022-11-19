46e3610bf295588539ba231928bb245cc8c864f2
satake91t <stk1293blue@gmail.com>
2022-11-19 19:09:33 +0900

    1119

--- app/src/main/java/com/example/quizapp/editPackFragment.java
+++ app/src/main/java/com/example/quizapp/editPackFragment.java
@@ -1,9 +1,11 @@
 package com.example.quizapp;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.RequiresApi;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -12,18 +14,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class editPackFragment extends Fragment implements View.OnClickListener {
 
     private mainApplication mainApp;
     private makePackActivity mpActivity;
-    private List<Integer> selectedQuizzes;
+    private List<Integer> selectedQuizzes = new ArrayList<>();
     private LinearLayout scrollLayout;
-    private Button btnDeletePack, btnDeleteQuiz, btnEditQuiz;
+    private Button btnResetSelection, btnDeletePack, btnDeleteQuiz, btnEditQuiz;
+    private ConstraintLayout deleteMsgBox;
+    private Button btnDeleteYes, btnDeleteNo;
 
     public editPackFragment() {
         // Required empty public constructor
@@ -58,14 +65,22 @@ public class editPackFragment extends Fragment implements View.OnClickListener {
         /*
         * ボタン初期設定
          */
+        this.btnResetSelection = view.findViewById(R.id.btnResetSelection);
+        this.btnResetSelection.setTag("resetSelection");
+        this.btnResetSelection.setEnabled(false);
+        this.btnResetSelection.setOnClickListener(this);
         this.btnDeletePack = view.findViewById(R.id.btnDeletePack);
         this.btnDeletePack.setTag("deletePack");
+        this.btnDeletePack.setOnClickListener(this);
         this.btnDeleteQuiz = view.findViewById(R.id.btnDeleteQuiz);
         this.btnDeleteQuiz.setTag("deleteQuiz");
-        this.btnDeleteQuiz.setActivated(false);
+        this.btnDeleteQuiz.setOnClickListener(this);
+        this.btnDeleteQuiz.setEnabled(false);
         this.btnEditQuiz = view.findViewById(R.id.btnEditQuizInEPF);
         this.btnEditQuiz.setTag("editQuiz");
-        this.btnEditQuiz.setActivated(false);
+        this.btnEditQuiz.setOnClickListener(this);
+        this.btnEditQuiz.setEnabled(false);
+        this.deleteMsgBox = view.findViewById(R.id.deleteMsgBox);
 
         /*
          * パック情報読み込み
@@ -95,12 +110,11 @@ public class editPackFragment extends Fragment implements View.OnClickListener {
          */
         List<String> lstPackIdFile = this.mainApp.readFileAsList(this.mainApp.getPackId());
         String[] lineIdFile = new String[6];
-        String quizSentence;
         scrollLayout = view.findViewById(R.id.quizListScroll);
         Button btnSelectQuiz;
+        CheckBox cbxSelectQuiz;
         for(int i = 0; i < lstPackIdFile.size(); i++){
             lineIdFile = lstPackIdFile.get(i).split(",");
-            quizSentence = lineIdFile[0];
             /*
             * ボタン作成
              */
@@ -108,6 +122,8 @@ public class editPackFragment extends Fragment implements View.OnClickListener {
             btnSelectQuiz.setText(lineIdFile[0]);
             btnSelectQuiz.setTextSize(30);
             btnSelectQuiz.setTag("quizSelect" + i);
+            btnSelectQuiz.setHighlightColor(Color.RED);
+            btnSelectQuiz.setBackgroundColor(Color.rgb(100,100,100));
             btnSelectQuiz.setOnClickListener(this);
             //ボタンの幅、高さの設定
             LinearLayout.LayoutParams buttonLayoutParams = new LinearLayout.LayoutParams(
@@ -115,7 +131,27 @@ public class editPackFragment extends Fragment implements View.OnClickListener {
                     LinearLayout.LayoutParams.WRAP_CONTENT);
             btnSelectQuiz.setLayoutParams(buttonLayoutParams);
             scrollLayout.addView(btnSelectQuiz);
+
+            /*cbxSelectQuiz = new CheckBox(this.mpActivity);
+            cbxSelectQuiz.setText(lineIdFile[0]);
+            cbxSelectQuiz.setTextSize(30);
+            cbxSelectQuiz.setTag("quizSelect" + i);
+            cbxSelectQuiz.setOnClickListener(this);
+            //ボタンの幅、高さの設定
+            LinearLayout.LayoutParams buttonLayoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            cbxSelectQuiz.setLayoutParams(buttonLayoutParams);
+            scrollLayout.addView(cbxSelectQuiz);*/
         }
+
+    }
+
+    public void reloadFragment(){
+        FragmentManager fm = getParentFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.container, new editPackFragment());
+        ft.commit();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -123,87 +159,90 @@ public class editPackFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view){
         if(view.getTag().toString().contains("quizSelect")){
             //クイズ選択ボタン
-            int quizNum = Integer.parseInt(view.getTag().toString().substring(10));
+            Integer quizNum = Integer.parseInt(view.getTag().toString().substring(10));
             if(this.selectedQuizzes.contains(quizNum)){
                 //選択済みの場合
                 this.selectedQuizzes.remove(this.selectedQuizzes.indexOf(quizNum));
+                view.setBackgroundColor(Color.rgb(100,100,100));
             }else{
                 //未選択の場合
-                this.selectedQuizzes.add(this.selectedQuizzes.indexOf(quizNum));
+                this.selectedQuizzes.add(quizNum);
+                view.setBackgroundColor(Color.rgb(70,70,70));
             }
 
             /*
             * 編集・削除ボタンの有効・無効化
              */
-            if(selectedQuizzes.size() == 0){
+            if(this.selectedQuizzes.size() == 0){
                 //編集・削除無効
-                this.btnDeleteQuiz.setActivated(false);
-                this.btnEditQuiz.setActivated(false);
-            }else if(selectedQuizzes.size() == 1){
+                this.btnResetSelection.setEnabled(false);
+                this.btnDeleteQuiz.setEnabled(false);
+                this.btnEditQuiz.setEnabled(false);
+            }else if(this.selectedQuizzes.size() == 1){
                 //編集・削除有効
-                this.btnDeleteQuiz.setActivated(true);
-                this.btnEditQuiz.setActivated(true);
+                this.btnResetSelection.setEnabled(true);
+                this.btnDeleteQuiz.setEnabled(true);
+                this.btnEditQuiz.setEnabled(true);
             }else{
                 //削除有効　編集無効
-                this.btnDeleteQuiz.setActivated(true);
-                this.btnEditQuiz.setActivated(false);
+                this.btnResetSelection.setEnabled(true);
+                this.btnDeleteQuiz.setEnabled(true);
+                this.btnEditQuiz.setEnabled(false);
             }
 
-        }else if(view.getTag().equals("back")){
-            /*
-            * 戻るボタン
-            * makePackActivityの再読み込み
-             */
-            mainApplication.setSelectPack(true);
-            this.mpActivity.reload();
-        }else if(view.getTag().equals("deletePack")){
+        }else if(view.getTag().toString().equals("deletePack")){
             /*
             * パック削除ボタン
-            * パックidファイルの削除
              */
+            //packIdファイル削除
             this.mainApp.clearFile(this.mainApp.getPackId());
-            List<String> lstPackDataFile = this.mainApp.getAllList();
-            for (String i:lstPackDataFile) {
-                String packId = i.substring(0,i.indexOf(","));
-                if(packId.equals(this.mainApp.getPackId())){
 
-                }
-            }
-            /*
-            * パックデータファイルの該当行削除
-             */
-            String linePackData;
+            //パックデータファイルの該当行削除
+            List<String> lstFileData = this.mainApp.getAllList();
+            String line;
             String packId;
-            for(int i = lstPackDataFile.size() - 1; i >= 0; i--){
-                linePackData = lstPackDataFile.get(i);
-                packId = linePackData.substring(0,linePackData.indexOf(","));
+            lstFileData = this.mainApp.readFileAsList(this.mainApp.PACK_DATA_FILE_NAME);
+            for(int i = 0; i < lstFileData.size(); i++){
+                line = lstFileData.get(i);
+                packId = line.substring(0,line.indexOf(","));
                 if(packId.equals(this.mainApp.getPackId())){
-                    lstPackDataFile.remove(i);
+                    lstFileData.remove(i);
+                    break;
                 }
             }
-            this.mainApp.saveFileByList(this.mainApp.PACK_DATA_FILE_NAME, lstPackDataFile);
+            this.mainApp.clearFile(this.mainApp.PACK_DATA_FILE_NAME);
+            this.mainApp.saveFileByList(this.mainApp.PACK_DATA_FILE_NAME, lstFileData);
 
             //activity再起動
-            mainApplication.setSelectPack(true);
             this.mpActivity.reload();
 
-        }else if(view.getTag().equals("deleteQuiz")){
+        }else if(view.getTag().toString().equals("deleteQuiz")){
             /*
             * クイズ削除ボタン
              */
+
+            //選択されたクイズ番号を降順ソート
+            Collections.sort(this.selectedQuizzes, Collections.reverseOrder());
+
+            //ファイルから該当行削除
             List<String> lstPackIdFile = this.mainApp.readFileAsList(this.mainApp.getPackId());
             for (int i:this.selectedQuizzes) {
                 lstPackIdFile.remove(i);
             }
+            this.mainApp.clearFile(this.mainApp.getPackId());
             this.mainApp.saveFileByList(this.mainApp.getPackId(), lstPackIdFile);
 
-            /*
-            * クイズリスト再読み込み
-             */
-            FragmentManager fm = getParentFragmentManager();
-            FragmentTransaction ft = fm.beginTransaction();
-            ft.replace(R.id.container, new editPackFragment());
-        }else if(view.getTag().equals("editQuiz")){
+            if(lstPackIdFile.size() == 0){
+                //activity再起動
+                //this.mpActivity.reload();
+                this.canDelete();
+            }else{
+                //クイズリスト再読み込み
+                //this.reloadFragment();
+                this.canDelete();
+            }
+
+        }else if(view.getTag().toString().equals("editQuiz")){
             /*
             * クイズ編集ボタン
              */
@@ -213,13 +252,26 @@ public class editPackFragment extends Fragment implements View.OnClickListener {
             FragmentTransaction ft = fm.beginTransaction();
             ft.addToBackStack(null);
             ft.replace(R.id.container, new editQuizFragment());
+            ft.commit();
+        }else if(view.getTag().toString().equals("resetSelection")){
+            this.reloadFragment();
         }
     }
 
 
     public boolean canDelete(){
+        //this.deleteMsgBox.setVisibility(View.VISIBLE);
+        YesNoDialog dialog = new YesNoDialog();
+        Bundle args = new Bundle();
+        args.putString("title", "警告");
+        args.putString("msg", "削除しますか?");
+        args.putString("Yes", "OK");
+        args.putString("No", "Cancel");
+        //dialog.setArguments(args);
+        //dialog.show(getChildFragmentManager(), "my_dialog");
         boolean isDeleteConfirmed=false;
-//toast
+
         return isDeleteConfirmed;
     }
+
 }
